ce6c8c5adb5efe0b8b50a65d7496b13365b622b3
ise user <ise@localhost.localdomain>
2020-05-05 16:00:54 +0000

    Initialization

---
+++ Initial_project/receiver.vhd
@@ -0,0 +1,87 @@
+library ieee;					-- klauzula dostepu do biblioteki 'IEEE'
+use     ieee.std_logic_1164.all;		-- dolaczenie calego pakietu 'STD_LOGIC_1164'
+use     ieee.std_logic_arith.all;		-- dolaczenie calego pakietu 'STD_LOGIC_ARITH'
+
+entity RECEIVER is				-- deklaracja sprzegu IEEE_CONV'
+  generic (
+	 word_len  :natural := 8 ;
+	 clock_speed :natural := 500_000_000;
+	 bod : natural := 9600;
+	 par_len  :natural := 1 ;
+	 stop_len : natural := 2;
+	 f_par : bit := '0'
+  );
+  port (
+    D		:in  bit_vector ( 0 to word_len - 1 );	-- wejscie danych 'D'
+	 C    :in  bit; --clock
+	 R    :in  bit; --clock
+	 ST   :in bit; --rozpoczecie nadawania 
+    U		:out bit;	-- wyjscie danych 'U'
+    VAL		:out bit;	-- wyjscie nadaje
+	 TX :in bit
+  );
+end RECEIVER;
+
+architecture cialo of RECEIVER is		-- deklaracja ciala 'cialo' architektury
+	type STATUSY is (czekaj, data, parzystosc, stop);
+	signal status : STATUSY;
+	signal S : natural range 0 to word_len;
+   signal PAR : natural range 0 to par_len;
+	signal STOP_P : natural range 0 to stop_len;
+	signal timer : natural range 0 to clock_speed/bod;
+	constant time_t : natural := clock_speed/bod;
+begin						-- poczatek czesci wykonawczej
+
+	
+  process (C, R) is
+   begin
+	if (R = '1') then
+			S <=word_len +1 ;
+			U <= '0';
+			STOP_P <= 0;
+			PAR <= 0;
+			status <= czekaj;
+	elsif (C'event and C='1') then 
+		if (timer /= time_t) then 
+			timer <= timer +1;
+		else
+		 timer <= 0;
+		if  (status = data) then
+			if ( S /= word_len ) then
+				VAL <= '1';
+				U <= D(S);
+				S <= S+1;
+			else
+				VAL <= '0';
+				U <= '0';
+				status <= parzystosc;
+				PAR <= 0;
+			end if;
+		elsif (status = parzystosc) then
+			if ( PAR /= par_len) then
+				VAL <= f_par;
+				PAR <= PAR+1;
+			else
+				PAR <= 0;			
+				status <= stop;
+			end if;
+		elsif (status = stop) then
+			if ( STOP_P /= stop_len) then
+				STOP_P <= STOP_P + 1;
+			else 
+				STOP_P <= 0;
+				VAL <= '0';
+				status <= czekaj;
+			end if;
+		elsif (status = czekaj and ST = '1') then
+			S <=word_len +1 ;
+			U <= '0';
+			STOP_P <= 0;
+			PAR <= 0;
+			status <= data;
+		end if;
+		end if;
+	end if;
+   end process ;
+
+end cialo;					-- zakonczenie ciala architektonicznego
---
+++ Initial_project/receiver_behav.vhd
@@ -0,0 +1,93 @@
+library ieee;
+use     ieee.std_logic_1164.all;
+use     ieee.std_logic_unsigned.all;
+use     ieee.std_logic_misc.all;
+
+entity RECEIVER_TB is
+  generic (
+    constant clock_speed   :natural := 20_000_000;		-- czestotliwosc zegara systemowego w [Hz]
+    constant bod       		:natural := 5_000_000;		-- predkosc nadawania w [bodach]
+    constant word_len      :natural := 8;			-- liczba bitow slowa danych (5-8)
+    constant par_len    	:natural := 1;			-- liczba bitow parzystosci (0-1)
+    constant stop_len      :natural := 2;			-- liczba bitow stopu (1-2)
+    constant N_RX          :boolean := FALSE;			-- negacja logiczna sygnalu szeregowego
+    constant N_SLOWO       :boolean := FALSE			-- negacja logiczna slowa danych
+  );
+end RECEIVER_TB;
+
+architecture behavioural of RECEIVER_TB is
+  
+  constant O_ZEGARA	:time := 1 sec/clock_speed;		-- okres zegara systemowego
+  constant O_BITU	:time := 1 sec/bod;			-- okres czasu trwania jednego bodu
+
+  signal   R		:std_logic := '0';			-- symulowany sygnal resetujacacy
+  signal   C		:std_logic := '1';			-- symulowany zegar taktujacy inicjowany na '1'
+  signal   TX		:std_logic;				-- symulowane wejscie 'RX'
+  signal   SLOWO	:std_logic_vector(B_SLOWA-1 downto 0);	-- obserwowane wyjscie 'SLOWO'
+  signal   GOTOWE	:std_logic;				-- obserwowane wyjscie 'GOTOWE'
+  signal   BLAD	:std_logic;				-- obserwowane wyjscie 'BLAD'
+  signal   D		:std_logic_vector(SLOWO'range);		-- symulowana dana transmitowana
+  
+begin
+
+ process is							-- proces bezwarunkowy
+  begin								-- czesc wykonawcza procesu
+    R <= '1'; wait for 100 ns;					-- ustawienie sygnalu 'res' na '1' i odczekanie 100 ns
+    R <= '0'; wait;						-- ustawienie sygnalu 'res' na '0' i zatrzymanie
+  end process;							-- zakonczenie procesu
+
+  process is							-- proces bezwarunkowy
+  begin								-- czesc wykonawcza procesu
+    C <= not(C); wait for O_ZEGARA/2;				-- zanegowanie sygnalu 'clk' i odczekanie pol okresu zegara
+  end process;							-- zakonczenie procesu
+  
+  process is							-- proces bezwarunkowy
+    function neg(V :std_logic; N :boolean) return std_logic is	-- deklaracja funkcji wewnetrznej 'neg'
+    begin							-- czesc wykonawcza funkcji wewnetrznej
+      if (N=FALSE) then return (V); end if;			-- zwrot wartosc 'V' gdy 'N'=FALSE
+      return (not(V));						-- zwrot zanegowanej wartosci 'V'
+    end function;						-- zakonczenie funkcji wewnetrznej
+  begin								-- czesc wykonawcza procesu
+    TX <= neg('0',N_RX);					-- incjalizacja sygnalu 'RX' na wartosci spoczynkowa
+    D  <= (others => '0');					-- wyzerowanie sygnalu 'D'
+    wait for 200 ns;						-- odczekanie 200 ns
+    loop							-- rozpoczecie petli nieskonczonej
+      TX <= neg('1',N_RX);					-- ustawienie 'RX' na wartosc bitu START
+      wait for O_BITU;						-- odczekanie jednego bodu
+      for i in 0 to word_len-1 loop				-- petla po kolejnych bitach slowa danych 'D'
+        TX <= neg(neg(D(i),N_SLOWO),N_RX);			-- ustawienie 'RX' na wartosc bitu 'D(i)'
+        wait for O_BITU;					-- odczekanie jednego bodu
+      end loop;							-- zakonczenie petli
+      if (par_len = 1) then				-- badanie aktywowania bitu parzystosci
+        TX<= neg(neg(XOR_REDUCE(D),N_SLOWO),N_RX);		-- ustawienie 'RX' na wartosc bitu parzystosci	
+        wait for O_BITU;					-- odczekanie jednego bodu
+      end if;							-- zakonczenie instukcji warunkowej
+      for i in 0 to stop_len-1 loop				-- petla po liczbie bitow STOP
+        TX <= neg('0',N_RX);					-- ustawienie 'RX' na wartosc bitu STOP
+        wait for O_BITU;					-- odczekanie jednego bodu
+      end loop;							-- zakonczenie petli
+      D <= D + 7;						-- zwiekszenia wartosci 'D' o 7
+      wait for 10*O_ZEGARA;					-- odczekanie 10-ciu okresow zegara
+    end loop;							-- zakonczenie petli
+  end process;							-- zakonczenie procesu
+  
+  RECEIVER_INST: entity work.RECEIVER				-- instancja odbiornika szeregowego 'SERIAL_RX'
+    generic map(						-- mapowanie parametrow biezacych
+      clock_speed     	 	=> clock_speed,				-- czestotliwosc zegata w [Hz]
+      bod             	 	=> bod,				-- predkosc odbierania w [bodach]
+      word_len         		=> word_len,				-- liczba bitow slowa danych (5-8)
+      par_len        		=> par_len,			-- liczba bitow parzystosci (0-1)
+      stop_len     	    	=> stop_len				-- liczba bitow stopu (1-2)
+  --  N_RX        	   	=> N_RX,				-- negacja logiczna sygnalu szeregowego
+  --  N_SLOWO        		=> N_SLOWO				-- negacja logiczna slowa danych
+    )
+    port map(							-- mapowanie sygnalow do portow
+      R                    => R,				-- sygnal resetowania
+      C                    => C,				-- zegar taktujacy
+      TX                   => TX,				-- odebrany sygnal szeregowy
+      SLOWO                => SLOWO,				-- odebrane slowo danych
+      GOTOWE               => GOTOWE,				-- flaga potwierdzenia odbioru
+      BLAD                 => BLAD				-- flaga wykrycia bledu w odbiorze
+    );
+
+end behavioural;
